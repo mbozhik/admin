0645a00401c3900c268cb174b46d157ed32daaf9
max <bozzhik@ya.ru>
2022-11-15 13:15:07 +0300

    confirmLogin *js

--- js/index.js
+++ js/index.js
@@ -1,75 +1,79 @@
+const confirmLogin = confirm('Log in as an administrator?')
+
+function processingLogin() {
+  if (confirmLogin == true) {
+    console.log('заходи, дядя админ')
+  } else {
+    return false
+  }
+}
+processingLogin()
+// console.log(confirmLogin)
+
+// setTimeout(function () {
+//   window.location.href = 'admin.html'
+// }, 6 * 1000)
+
+function renderIndex() {}
 // get the element
-const text = document.querySelector('.text-console');
+const text = document.querySelector('.text-console')
 
 // make a words array
-const words = [
-  "run..",
-  "loading..",
-  "initialize..",
-  "log in.."
-];
+const words = ['run..', 'loading..', 'initialize..', 'log in..']
 
 // start typing effect
-setTyper(text, words);
+setTyper(text, words)
 
 function setTyper(element, words) {
+  const LETTER_TYPE_DELAY = 50
+  const WORD_STAY_DELAY = 500
 
-  const LETTER_TYPE_DELAY = 50;
-  const WORD_STAY_DELAY = 500;
+  const DIRECTION_FORWARDS = 0
+  const DIRECTION_BACKWARDS = 1
 
-  const DIRECTION_FORWARDS = 0;
-  const DIRECTION_BACKWARDS = 1;
+  var direction = DIRECTION_FORWARDS
+  var wordIndex = 0
+  var letterIndex = 0
 
-  var direction = DIRECTION_FORWARDS;
-  var wordIndex = 0;
-  var letterIndex = 0;
+  var wordTypeInterval
 
-  var wordTypeInterval;
-
-  startTyping();
+  startTyping()
 
   function startTyping() {
-    wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
+    wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY)
   }
 
   function typeLetter() {
-    const word = words[wordIndex];
+    const word = words[wordIndex]
 
     if (direction == DIRECTION_FORWARDS) {
-      letterIndex++;
+      letterIndex++
 
       if (letterIndex == word.length) {
-        direction = DIRECTION_BACKWARDS;
-        clearInterval(wordTypeInterval);
-        setTimeout(startTyping, WORD_STAY_DELAY);
+        direction = DIRECTION_BACKWARDS
+        clearInterval(wordTypeInterval)
+        setTimeout(startTyping, WORD_STAY_DELAY)
       }
-
     } else if (direction == DIRECTION_BACKWARDS) {
-      letterIndex--;
+      letterIndex--
 
       if (letterIndex == 0) {
-        nextWord();
+        nextWord()
       }
     }
 
-    const textToType = word.substring(0, letterIndex);
+    const textToType = word.substring(0, letterIndex)
 
-    element.textContent = textToType;
+    element.textContent = textToType
   }
 
   function nextWord() {
-
-    letterIndex = 0;
-    direction = DIRECTION_FORWARDS;
-    wordIndex++;
+    letterIndex = 0
+    direction = DIRECTION_FORWARDS
+    wordIndex++
 
     if (wordIndex == words.length) {
-      wordIndex = 4;
+      wordIndex = 4
     }
-
   }
 }
-
-setTimeout(function(){
-    window.location.href = 'admin.html';
-  }, 6 * 1000);
