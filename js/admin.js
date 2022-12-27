2829cf5ce351b7e130448b47df081d451d932330
max <bozzhik@ya.ru>
2022-12-28 02:10:37 +0300

    upgrade code
    change scss to css variables
    remove broken script

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,7 @@
-let container = document.querySelector('.container')
-let canvas = document.querySelector('.canvas')
-container.addEventListener('mousemove', (e) => {
-   let x = e.clientX - container.getBoundingClientRect().left
-   let y = e.clientY - container.getBoundingClientRect().top
-   canvas.style.transform = `translate(-${x}px, -${y * 1.5}px)`
-})
+// let container = document.querySelector('.container')
+// let canvas = document.querySelector('.canvas')
+// container.addEventListener('mousemove', (e) => {
+//    let x = e.clientX - container.getBoundingClientRect().left
+//    let y = e.clientY - container.getBoundingClientRect().top
+//    canvas.style.transform = `translate(-${x}px, -${y * 1.5}px)`
+// })
