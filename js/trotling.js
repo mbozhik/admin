8da03240995ee49824fb4fdfbed312547cae2466
max <bozzhik@ya.ru>
2023-01-04 17:15:11 +0300

    make trotling script

--- js/trotling.js
+++ js/trotling.js
@@ -1,33 +1,24 @@
-const program = document.getElementById('program');
-const fix = document.getElementById('program-fix');
-const next = document.getElementById('next-machine');
+const images = document.getElementsByClassName('program');
 
-fix.style.visibility = 'hidden';
-
-document.addEventListener('mousemove', function (event) {
-   program.style.top = event.clientY - 225 + 'px';
-   program.style.left = event.clientX - 225 + 'px';
-});
-
-function endStick() {
-   setTimeout(() => {
-      program.style.opacity = '0.5';
-      fix.style.visibility = 'visible';
-      fix.src = 'img/stick-fix.gif';
-   }, 500);
-
-   setTimeout(() => {
-      program.style.opacity = 0;
-   }, 3000);
-
-   setTimeout(() => {
-      fix.style.visibility = 'hidden';
-   }, 3900);
-
-   setInterval(() => {
-      next.classList.add('scaled');
-   }, 4100);
+function showImages() {
+   for (let i = 0; i < images.length; i++) {
+      setTimeout(() => {
+         // Generate a random number between 0 and 1
+         const x = Math.random();
+         // Generate a random number between 0 and 1
+         const y = Math.random();
+         // Set the image's position based on the random numbers
+         images[i].style.top = `${x * 100}%`;
+         images[i].style.left = `${y * 100}%`;
+         // Make the image visible
+         images[i].style.opacity = 1;
+      }, i * 300);
+   }
 }
 
-const button = document.getElementById('fix-this');
-button.addEventListener('click', endStick);
+showImages();
+
+document.addEventListener('click', function (event) {
+   event.target.style.display = 'none';
+   showImages();
+});
